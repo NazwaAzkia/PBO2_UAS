7fe65d4d9f8423cf239e0203a344ffb93a2a5277
NazwaAzkia <nazwaazkia2004@gmail.com>
2025-01-16 14:02:26 +0700

    Add files via upload

---
+++ ujikoding_2210010396/src/ujikoding_2210010396/Ujikoding_2210010396.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ujikoding_2210010396;
+import sys.crudTable;
+import javax.swing.JOptionPane;
+import java.util.Scanner;
+import sys.crudTable;
+import GUI.FrameKHS;
+/**
+ *
+ * @author ASUS
+ */
+public class Ujikoding_2210010396 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        new FrameKHS().setVisible(true);
+    }
+    
+}
