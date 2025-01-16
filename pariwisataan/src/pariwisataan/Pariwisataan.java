4f07e81c06d29cb2a5036156a1e6466a29a4ec5a
NazwaAzkia <nazwaazkia2004@gmail.com>
2025-01-16 14:01:03 +0700

    Add files via upload

---
+++ pariwisataan/src/pariwisataan/Pariwisataan.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pariwisataan;
+import sys.crudTable;
+import javax.swing.JOptionPane;
+import java.util.Scanner;
+import sys.crudTable;
+import GUI.FrameUtama;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Pariwisataan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        new FrameUtama().setVisible(true);
+    }
+    
+}
